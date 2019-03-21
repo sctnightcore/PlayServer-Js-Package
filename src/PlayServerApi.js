2f8e637382e866b2a57ddffdd5a098943bfc6da2
sctnightcore <23263315+sctnightcore@users.noreply.github.com>
2019-03-21 09:05:04 +0700

    Update PlayServerApi.js

--- src/PlayServerApi.js
+++ src/PlayServerApi.js
@@ -31,22 +31,21 @@ module.exports.save = {
 
 module.exports.send = {
 	checksum: async function(url, serverid, answer, gameid, checksum) {
-		try {
-          var data = `server_id=${serverid}&captcha=${answer}&gameid=${gameid}&checksum=${checksum}`;
-          var headers = {
-              'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
-              'Referer': `http://playserver.in.th/index.php/Vote/prokud/${url}`
-          };
-        	let out = await axios({
-        		method: 'POST',
-        		url: `http://playserver.co/index.php/Vote/ajax_submitpic/${url}`,
-        		data: data,
-        		headers: headers
-       		});
-       		return out.data;
-       	} catch (err) {
-       		return err.data;
+	try {
+          	var data = `server_id=${serverid}&captcha=${answer}&gameid=${gameid}&checksum=${checksum}`;
+		var headers = {
+			'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
+			'Referer': `http://playserver.in.th/index.php/Vote/prokud/${url}`
+		};
+		let out = await axios({
+			method: 'POST',
+			url: `http://playserver.co/index.php/Vote/ajax_submitpic/${url}`,
+			data: data,
+			headers: headers
+		});
+		return out.data;
+	} catch (err) {
+	return err.data;
        	}
-	}	
 }
 
